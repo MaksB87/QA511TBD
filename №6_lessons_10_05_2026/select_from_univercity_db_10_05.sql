90c71f9f1249a573aba010fc4f3f6e6331178388
Max Bakhtin <maximbakhtin83@gmail.com>
2026-05-10 11:48:10 +0300

    подзапросы часть 1

--- №6_lessons_10_05_2026/select_from_univercity_db_10_05.sql
+++ №6_lessons_10_05_2026/select_from_univercity_db_10_05.sql
@@ -10,3 +10,58 @@ from [Groups] as G, Students S where G.id=S.Groupid
 group by GroupName having 
 count(S.groupid)>1 order by 
 [кол-во студентов] desc; 
+
+-- select * from Teachers;
+select *from students;
+-- select *from achievements;
+-- select * from Subjects;
+-- select*from [groups];
+
+
+select T.Firstname||' '||T.LastName [Teacher Full Name], St.lastname,st.firstname
+from Teachers T,
+ Teachers_Subjects TS,Achievements A, Subjects S,
+Students ST where T.Id=TS.Teacherid and
+TS.subjectid=S.id and S.id=A.Subjectid 
+and A.studentid=ST.id
+;
+
+-- !!!!
+-- select T.Firstname||' '||T.LastName [Teacher Full Name]
+-- from Teachers T,
+--  Teachers_Subjects TS,Achievements A, Subjects S,
+-- Students ST where T.Id=TS.Teacherid and
+-- TS.subjectid=S.id and S.id=A.Subjectid and A.studentid=ST.id
+-- group by  [Teacher Full Name] having ST.Lastname in ('Thomas','Miller','Evans','Jones') ;
+-- !!!
+
+select id,firstname,lastname from students
+ group by firstname,lastname having lastname in ('Moore','Thomas','Brown');
+
+
+select T.Firstname||' '||T.LastName [Teacher Full Name]  from Teachers T,
+ Teachers_Subjects TS,Achievements A, Subjects S where T.Id=TS.Teacherid and
+TS.subjectid=S.id and S.id=A.Subjectid and A.studentid in (select id from students
+ group by firstname,lastname having lastname in ('Moore','Thomas','Evans')) ;
+
+
+-- !!!
+select  firstname,lastname,groupname,grants 
+from students s, [groups] g where
+ s.groupid=g.id group by 
+  firstname,lastname,groupname,
+ grants having grants=max(grants) ;
+
+ select  firstname,lastname,groupname,grants 
+from students s, [groups] g where
+ s.groupid=g.id group by 
+  firstname,lastname,groupname,
+ grants having grants=1256.0;
+
+-- select max(grants) from students;
+select  firstname,lastname,groupname,grants 
+from students s, [groups] g where
+ s.groupid=g.id and grants=
+ (select max(grants) from students);
+
+
---
+++ №6_lessons_10_05_2026/select_from_univercity_db_part_2.sql
@@ -0,0 +1,5 @@
+select id,groupname from [groups]  where groupname like '%11';  
+
+!!!
+-- select firstname||' '||lastname FullName,Grants from students s where
+--  groupid=(select id from [groups]  where groupname like '%11');
